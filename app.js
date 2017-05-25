2b8330ee164274a0105e51bddd02f2175f97a245
Peter Szujewski <pszujewski@gmail.com>
2017-05-25 17:24:46 -0400

    Started cleaning code, finished heat map

--- app.js
+++ app.js
@@ -1,3 +1,114 @@
+// Define helper functions for common tasks
+
+function getD3Canvas(root, type, width, height) {
+  // extra vertical padding needed for vertical bar chart 
+  height = type === "horizontal" ? height : height + 50;
+  const canvas = d3.select(root)
+    .append("svg")
+    .attr("class", "canvas")
+    .attr("width", width)
+    .attr("height", height)
+    .append("g");
+  if (type === "horizontal") {
+    canvas.attr("class", "bars-group").attr("transform", "translate(61, 20)");
+  }
+  return canvas;
+}
+
+function getlinearScale(domain, range) {
+  return d3.scaleLinear().domain(domain).range(range);
+}
+
+function getBandScale(domain, range) {
+  return d3.scaleBand().domain(domain).range(range); 
+}
+
+function makeHorizontalBars(canvas, data, xScale, xValue, yScale, yValue) {
+  return canvas.selectAll("rect")
+    .data(data).enter()
+      .append("rect")
+      .attr("fill", "#3e4958")
+      .transition().delay(500).duration(800)
+        .attr("width", d => xScale(d[xValue]))
+        .attr("height", 50)
+        .attr("y", d => yScale(d[yValue]));
+}
+
+function makeVerticalBars(canvas, data, height, xScale, xValue, yScale, yValue) {
+  return canvas.selectAll("rect")
+    .data(data).enter()
+      .append("rect")
+      .attr("fill", "#3e4958")
+      .transition().delay(500).duration(800)
+        .attr("width", 50)
+        .attr("x", d => xScale(d[xValue]) + 53) 
+        .attr("y", d => yScale(d[yValue])) 
+        .attr("height", d => height - yScale(d[yValue])); 
+}
+
+// Define functions for displaying D3 charts
+
+function makeCanvasGetAxis(root, type, data, width, height) {
+
+  let xScale;
+  let xValue;
+  let yScale;
+  let yValue
+
+  if (type === "horizontal") { 
+    xScale = getlinearScale([0, max+3], [0, width]);
+    xValue = "percentPop";
+    yScale = getBandScale(languages.map(item => item.language), [0, height-75]);
+    yValue = "language";
+  } else {
+    xScale = getBandScale(languages.map(item => item.language), [0, width]);
+    xValue = "language";
+    yScale = getlinearScale([0, max+3], [height, 0]);
+    yValue = "percentPop";
+  }
+
+  const canvas = getD3Canvas(root, type, width, height); 
+
+  let bars;
+  if (type === "horizontal") {
+    bars = makeHorizontalBars(canvas, data, xScale, xValue, yScale, yValue);
+  } else {
+    bars = makeVerticalBars(canvas, data, height, xScale, xValue, yScale, yValue);
+  }
+
+  return { canvas, xScale, xValue, yScale, yValue };
+}
+
+function doHorizontalBarChart(root, type, data, width, height) {
+
+  const { canvas, xScale, xValue, yScale, yValue } = makeCanvasGetAxis(root, type, data, width, height);
+
+  canvas.selectAll(".text")
+    .data(data).enter()
+      .append("text")
+      .transition().delay(1300).duration(0)
+        .attr("class","label")
+        .attr("x", d => xScale(d[xValue]) + 5)
+        .attr("y", d => yScale(d[yValue]) + 32)
+        .text(d => d[xValue] + "%" );  
+  
+  canvas.append("g")
+    .attr("class", "axis --axis-x")
+    .attr("transform", "translate(0,"+String(height-70)+")")
+    .call(d3.axisBottom(xScale).tickFormat(d => d + "%"));                   
+
+  canvas.append("g")
+    .attr("class", "axis --axis-y")
+    .attr("transform", "translate(-6, 0)") 
+    .call(d3.axisLeft(yScale));
+
+}
+
+// ================================ OLD ================================
+// ================================ OLD ================================
+// ================================ OLD ================================
+// ================================ OLD ================================
+
 $(function() {
       $( "#tabs" ).tabs();
 });
@@ -60,12 +171,12 @@ getData().then(data => {
 
 function doHorizontalBarChart(root) {
 
-  d3.select(root).append("p").attr("class", "section-header")
-    .text("Languages spoken in the U.S. other than English (expressed as % of the total population)");
+  // d3.select(root).append("p").attr("class", "section-header")
+  //   .text("Languages spoken in the U.S. other than English (expressed as % of the total population)");
 
   const width = 700;
   const height = 400;
-  padding = 30;
+  const padding = 30;
 
   const xScale = d3.scaleLinear()
     .domain([0, max + 3])
@@ -78,9 +189,9 @@ function doHorizontalBarChart(root) {
   const canvas = d3.select(root)
     .append("svg")
     .attr("class", "canvas")
-    .attr("width", width + padding)
+    .attr("width", width)
     .attr("height", height)
-    .attr("padding", padding)
+    //.attr("padding", padding)
       .append("g")
       .attr("class", "bars-group")
       .attr("transform", "translate(61, 20)");
@@ -285,7 +396,6 @@ else {
           const percentEval = d.percent / total;
           return percentEval.toFixed(1); 
         } else {
-          console.log(d);
           return d.percent;
         }
       });
@@ -319,38 +429,118 @@ function loadMap(root) {
   const width = 960;
   const height = 600;
 
-  // const width = 420;
-  // const height = 400;
+  const highColor = "#d73d32";
+  const lowColor = "#f7d8d6";
 
-  // const projection = d3.geoAlbersUsa()
-  //   .scale(170)
-  //   .translate([width/2, height/2]);
+  // D3 Projection
+  const projection = d3.geoAlbersUsa()
+  .translate([width / 2, height / 2]) // translate to center of screen
+  .scale([1000]); // scale things down so see entire US
+
+// Define path generator
+  const path = d3.geoPath() // path generator that will convert GeoJSON to SVG paths
+    .projection(projection); // tell path generator to use albersUsa projection
 
   const svg = d3.select(root).append("svg")
     .attr("width", width)
-    .attr("height", height);
+    .attr("height", height)
+    .attr("transform", "translate(60, 0)");
 
-  const path = d3.geoPath();
+  d3.csv("./states-data.csv", function(error, data) {
+    // Get the data on the states
+    console.log(data);
+    let dataArray = [];
+    for (let d = 0; d < data.length; d++) {
+      dataArray.push(parseFloat(data[d].percentPopNonEng));
+    }
+    const max = d3.max(dataArray);
+    const min = d3.min(dataArray);
 
-  d3.json("https://d3js.org/us-10m.v1.json", function(error, us) {
+    const colorScale = d3.scaleLinear()
+      .domain([min,max])
+      .range([lowColor, highColor]);
     
-    if (error) throw error;
+    // Load GeoJSON data and merge with states data
+    d3.json("./us-states.json", function(error, json) {
+      
+      if (error) throw error;
 
-    console.log(us);
+      for (let i=0; i<data.length; i++) {
+        // Grab State Name and data value (percentPop)
+        const dataState = data[i].state;
+        const dataPercentage = data[i].percentPopNonEng;
 
-    svg.append("g")
-      .attr("class", "states")
-      .selectAll("path")
-      .data(topojson.feature(us, us.objects.states).features)
-      .enter()
-      .append("path")
-        .attr("d", path);
+        // Find the corresponding state inside the GeoJSON
+        for (let j = 0; j < json.features.length; j++) {
+          const jsonState = json.features[j].properties.name;
 
-    svg.append("path")
-        .attr("class", "state-borders")
-        .attr("d", path(topojson.mesh(us, us.objects.states, function(a, b) { return a !== b; })));
+          if (dataState == jsonState) {
+            // Copy the data value into the JSON
+            json.features[j].properties.percentPopNonEng = dataPercentage;
+            // Stop looking through the JSON
+            break;
+          }
+        }
+      }
 
-  });    
+      svg.selectAll("path")
+        .data(json.features)
+        .enter()
+        .append("path")
+          .attr("d", path)
+          .attr("stroke", "#fff")
+          .attr("stroke-width", "1")
+          .attr("fill", function(d) { return colorScale(d.properties.percentPopNonEng) });
+
+      // add a legend
+		  const w = 140
+      const h = 300;
+
+      const key = d3.select(root)
+        .append("svg")
+        .attr("width", w)
+        .attr("height", h)
+        .attr("class", "legend");
+
+      legend = key.append("defs")
+			.append("svg:linearGradient")
+			.attr("id", "gradient")
+			.attr("x1", "100%")
+			.attr("y1", "0%")
+			.attr("x2", "100%")
+			.attr("y2", "100%")
+			.attr("spreadMethod", "pad");
+
+		legend.append("stop")
+			.attr("offset", "0%")
+			.attr("stop-color", highColor)
+			.attr("stop-opacity", 1);
+			
+		legend.append("stop")
+			.attr("offset", "100%")
+			.attr("stop-color", lowColor)
+			.attr("stop-opacity", 1);
+
+		key.append("rect")
+			.attr("width", w - 100)
+			.attr("height", h)
+			.style("fill", "url(#gradient)")
+			.attr("transform", "translate(0,10)");
+
+		const y = d3.scaleLinear()
+      .domain([min, max])
+			.range([h, 0]);
+
+		const yAxis = d3.axisRight(y).tickFormat(d => d + "%");
+
+		key.append("g")
+			.attr("class", "y axis")
+			.attr("transform", "translate(41,10)")
+			.call(yAxis)
+
+    });   
+
+  });
 
 }
 
